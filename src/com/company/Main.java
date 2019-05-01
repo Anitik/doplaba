002358fe1ffbf3c47dfcef47eb5bb6dbbfb25f08
Anitik <Anita15@ukr.net>
2019-05-01 19:50:44 +0300

    ....

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,15 +9,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-     System.out.println("-ЗАДАЧА 5-");
+        System.out.println("-ЗАДАЧА 5-");
 
         /*У заданій послідовності цілих чисел знайти максимально довгу підпослідовність чисел таку,
                 що кожний наступний елемент підпослідовності ділився без остатку на попередній. */
 
         System.out.println("Введите количество чисел: ");
         int n = in.nextInt(); // 10
-        int k=0,j=0;
-        int [] myArray = new int[n];
+        int k = 0, j = 0;
+        int[] myArray = new int[n];
         StringBuilder s = new StringBuilder("");
         StringBuilder s2 = new StringBuilder("");
 
@@ -26,34 +26,31 @@ public class Main {
             myArray[i] = in.nextInt(); // 2, 2, 2, 5, 5, 5, 5, 5, 5, 6
         }
 
-        for (int i=0; i<n-1; i++){
+        for (int i = 0; i < n - 1; i++) {
 
-            if (myArray[i+1]%myArray[i]==0) {
+            if (myArray[i + 1] % myArray[i] == 0) {
 
-                s.append(myArray[i+1]+ " ");
+                s.append(myArray[i + 1] + " ");
                 k++;
 
-                if (myArray[i+1]==myArray[myArray.length-1]){
-                    if(k>j) {
+                if (myArray[i + 1] == myArray[myArray.length - 1]) {
+                    if (k > j) {
                         s2.delete(0, s2.length());
                         s2.append(s);
                     }
                 }
-            }
-
-            else {
-                if(k>j){
+            } else {
+                if (k > j) {
                     s2.delete(0, s2.length());
                     s2.append(s);
-                    j=k;
-                    k=0;
-                     s.delete(0, s.length()-1);
+                    j = k;
+                    k = 0;
+                    s.delete(0, s.length() - 1);
 
-                }
-                else {
+                } else {
 
-                    k=0;
-                    s.delete(0, s.length()-1);
+                    k = 0;
+                    s.delete(0, s.length() - 1);
 
                 }
 
@@ -73,53 +70,118 @@ public class Main {
 
         System.out.println("Введите количество посетителей: ");
         int m = in.nextInt(); // 10
-        int people [][] = new int[m][2];
-        int time [] = {10, 11, 12, 13, 14, 15, 16, 17, 18};
-        int count [] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int people[][] = new int[m][2];
+        int time[] = {10, 11, 12, 13, 14, 15, 16, 17, 18};
+        int count[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
         System.out.println("Музей работает с 11 до 18 часов. Время вводится целыми числами.");
-        for (int i=0; i<m; i++){
-            System.out.println("Введите время прихода и ухода "+(i+1)+"-го посетителя:");
-            people[i][0]=in.nextInt();
-            people[i][1]=in.nextInt();
+        for (int i = 0; i < m; i++) {
+            System.out.println("Введите время прихода и ухода " + (i + 1) + "-го посетителя:");
+            people[i][0] = in.nextInt();
+            people[i][1] = in.nextInt();
 
         }
 
-        for (int i=0; i<m; i++){
-            for (int l=people[i][0]; l<=people[i][1]; l++){
-                for(int r=0; r<time.length; r++){
-                    if (l==time[r]){
-                        count[r]+=1;
+        for (int i = 0; i < m; i++) {
+            for (int l = people[i][0]; l <= people[i][1]; l++) {
+                for (int r = 0; r < time.length; r++) {
+                    if (l == time[r]) {
+                        count[r] += 1;
                     }
                 }
             }
         }
 
 
-        for(int i = count.length-1 ; i > 0 ; i--){
-            for(int y = 0 ; y < i ; y++){
-            if( count[y] < count[y+1] ){
+        for (int i = count.length - 1; i > 0; i--) {
+            for (int y = 0; y < i; y++) {
+                if (count[y] < count[y + 1]) {
 
-                int tmp2 = time[y];
-                time[y] = time[y+1];
-                time[y+1] = tmp2;
+                    int tmp2 = time[y];
+                    time[y] = time[y + 1];
+                    time[y + 1] = tmp2;
 
-                int tmp = count[y];
-                count[y] = count[y+1];
-                count[y+1] = tmp;
+                    int tmp = count[y];
+                    count[y] = count[y + 1];
+                    count[y + 1] = tmp;
+                }
+            }
+        }
+
+        System.out.println("Самое большое количество посетителей находилось в музее в " + time[0] + " часов - " + count[0] + " человек(-а).");
+
+
+        for (int i = 1; i < time.length; i++) {
+            System.out.println("В " + time[i] + " часов в музее находилось " + count[i] + " человек(-а).");
+        }
+
+
+        System.out.println("-ЗАДАЧА 9-");
+
+          /*Дан масив X [1..N]. Необхідно циклічно зрушити його на k елементів вправо
+          (тобто елемент X [i] після зсуву повинен стояти на місці X [i + k];
+          тут ми вважаємо що за X [N] слід X [1]). Додаткового масиву заводити не можна! */
+
+
+        System.out.println("Введите длину массива: ");
+        int b;
+        b = in.nextInt();
+        int arr[] = new int[b];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+            System.out.print(arr[i] + " ");
+
+        }
+        System.out.println();
+        System.out.println("Введите k: ");
+        int e = in.nextInt();
+        moveRight(arr, e);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+
+        }
+
+
+        System.out.println("-ЗАДАЧА 17-");
+
+        /*Вводиться послідовність з n натуральних чисел.
+        Необхідно визначити найменше натуральне число, якого немає у послідовності. */
+
+
+        System.out.println("Введите количество чисел: ");
+        int g = in.nextInt();
+        int ar[] = new int[g];
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = (int) ((Math.random() * 99) + 1);
+            System.out.print(ar[i] + " ");
+
+        }
+        int min = ar[0];
+        for (int i = 0; i < ar.length; i++) {
+            if (ar[i] < min) {
+                min = ar[i];
+            }
+        }
+        System.out.println("min=" + min);
+        for (int i = 1; i < min; i++) {
+            if (i != min) {
+                System.out.println("Минимальное натуральное число которого нет:" + i);
+                break;
+
+
             }
         }
     }
 
-        System.out.println("Самое большое количество посетителей находилось в музее в "+time[0]+" часов - "+count[0]+" человек(-а).");
-
-
-        for (int i=1; i<time.length; i++){
-            System.out.println("В "+time[i]+" часов в музее находилось "+count[i]+" человек(-а).");
+    public static void moveRight(int[] array, int positions) {
+        int size = array.length;
+        for (int i = 0; i < positions; i++) {
+            int temp = array[size - 1];
+            for (int j = size - 1; j > 0; j--) {
+                array[j] = array[j - 1];
+            }
+            array[0] = temp;
         }
 
-
-
-
-     }
+    }
 }
