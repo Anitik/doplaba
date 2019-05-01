4909a54561a1c885e3a2b6c4015493ec1dede773
Anitik <Anita15@ukr.net>
2019-05-01 18:04:33 +0300

    ....

---
+++ src/com/company/Main.java
@@ -0,0 +1,78 @@
+package com.company;
+
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("-ЗАДАЧА 5-");
+
+        /*У заданій послідовності цілих чисел знайти максимально довгу підпослідовність чисел таку,
+                що кожний наступний елемент підпослідовності ділився без остатку на попередній.*/
+
+        System.out.println("Введите количество чисел: ");
+        int n = in.nextInt(); // 10
+        int k=0,j=0;
+        int [] myArray = new int[n];
+        StringBuilder s = new StringBuilder("");
+        StringBuilder s2 = new StringBuilder("");
+
+        System.out.println("Введите числа: ");
+        for (int i = 0; i < n; i++) {
+            myArray[i] = in.nextInt(); // 2, 2, 2, 5, 5, 5, 5, 5, 5, 6
+        }
+
+        for (int i=0; i<n-1; i++){
+
+            if (myArray[i+1]%myArray[i]==0) {
+
+                s.append(myArray[i+1]+ " ");
+                k++;
+
+                if (myArray[i+1]==myArray[myArray.length-1]){
+                    if(k>j) {
+                        s2.delete(0, s2.length());
+                        s2.append(s);
+                    }
+                }
+            }
+
+            else {
+                if(k>j){
+                    s2.delete(0, s2.length());
+                    s2.append(s);
+                    j=k;
+                    k=0;
+                     s.delete(0, s.length()-1);
+
+                }
+                else {
+
+                    k=0;
+                    s.delete(0, s.length()-1);
+
+                }
+
+            }
+
+        }
+        System.out.println("Самая длинная подпоследовательность последовательности :");
+        System.out.println(s2);
+
+        System.out.println("-ЗАДАЧА 8-");
+
+/*
+        У музеї реєструється протягом дня час приходу і відходу кожного відвідувача.
+        Таким чином за день отримані N пар значень, де перше значення у парі показує час приходу відвідувача
+        і друге значення - час його відходу. Знайти проміжок часу, протягом якого в музеї одночасно перебувало
+        максимальне число відвідувачів.*/
+
+
+
+
+     }
+}
